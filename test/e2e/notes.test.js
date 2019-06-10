d1056139b6f19a98a77ba0b57d2de3d042696ea4
Tommy Tran <tranttommy95@gmail.com>
2019-06-10 14:21:55 -0700

    made get route

--- test/e2e/notes.test.js
+++ test/e2e/notes.test.js
@@ -21,4 +21,24 @@ describe('notes routes', () => {
         });
       });
   });
+
+  it('creates all notes', async () => {
+    await request(app)
+      .post('/api/v1/notes')
+      .send({
+        title: 'My Cool Title',
+        body: 'My Cool Body',
+        comment: 'My Cool Comment'
+      });
+
+    const res = await request(app).get('/api/v1/notes');
+    expect(res.body).toEqual([
+      {
+        _id: expect.any(String),
+        title: 'My Cool Title',
+        body: 'My Cool Body',
+        __v: 0
+      }
+    ]);
+  });
 });
